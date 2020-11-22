9e84ae43e0ce0b6518a202ec155fdcd870fcdeff
Gabriel Pineda <gabepineda6@gmail.com>
2020-11-21 23:37:23 -0800

    created the search function

--- book-search/client/src/components/Jumbotron.js
+++ book-search/client/src/components/Jumbotron.js
@@ -4,11 +4,11 @@ function Jumbo () {
   return (
     <div className="container">
         <br></br>
-        <div class="jumbotron">
+        <div className="jumbotron">
             
-            <h1 class="display-4">(React) Google Book Search</h1>
-            <h3 class="lead">Search for and Save books of interest.</h3>
-            <hr class="my-4"></hr>
+            <h1 className="display-4">(React) Google Book Search</h1>
+            <h3 className="lead">Search for and Save books of interest.</h3>
+            <hr className="my-4"></hr>
         </div>
     </div>
   )
--- book-search/client/src/components/search.js
+++ book-search/client/src/components/search.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
+import API from '../utils/API'
+import Items from '../components/List/Items'
 
-function Search () {
-  return (
-    <div class="container">
-        <h3>Search a Book</h3>
-        <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="button-addon2"></input>
-        <button class="btn btn-outline-secondary mt-3" type="button" id="button-addon2">Search</button>
-    </div>
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: '',
+        books:[]
+    };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+    handleChange(event){
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+        API.getBooks(this.state.value).then(res => this.setState(state =>{
+            return {books: res.data.items}
+        }))
+    }
+
+    render() {return (
+        <div className="container">
+            <h3>Search a Book</h3>
+            <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+            <button className="btn btn-outline-secondary mt-3 mb-3" onClick={this.handleSubmit} type="button" id="button-addon2">Search</button>
+            {this.state.books.map(items => 
+                <Items title={items.volumeInfo.title} author={items.volumeInfo.authors} description={items.volumeInfo.description} link={items.volumeInfo.infoLink} image={items.volumeInfo.imageLinks.smallThumbnail} />  
+                )}           
+        </div>
   )
+    }
 }
 
 export default Search;
